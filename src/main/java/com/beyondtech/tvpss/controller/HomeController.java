b12d733a82b8e70a93ac102ba6e4db31528ab57b
Adam Imran Bin Alwi <adamimran812@gmail.com>
2025-01-04 20:40:10 +0800

    Initial commit

---
+++ src/main/java/com/beyondtech/tvpss/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.beyondtech.tvpss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+
+		return "index";
+	}
+
+	@GetMapping("/dashboard")
+	public String dashboard(Model model) {
+
+		return "dashboard";
+	}
+
+	@GetMapping("/apply")
+	public String apply(Model model) {
+		return "apply";
+	}
+
+	@GetMapping("/apply/detail")
+	public String applicationdetail(Model model) {
+		return "application-detail";
+	}
+
+}
