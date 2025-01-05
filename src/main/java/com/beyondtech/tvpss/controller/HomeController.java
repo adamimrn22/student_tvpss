2320f8cefe63c228e907bffccf7c9bb71c07f8d8
Adam Imran Bin Alwi <adamimran812@gmail.com>
2025-01-05 12:16:43 +0800

    added login and seperate file into component

--- src/main/java/com/beyondtech/tvpss/controller/HomeController.java
+++ src/main/java/com/beyondtech/tvpss/controller/HomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
+
 	@GetMapping("/")
 	public String index(Model model) {
 
